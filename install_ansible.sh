6d0aed918cab8a60e5959966e12101610b920221
Krishna <krishna@agentelinux.com.br>
2020-03-16 16:43:21 -0300

    ajustes para windows

--- install_ansible.sh
+++ install_ansible.sh
@@ -5,7 +5,6 @@ sudo apt-get install software-properties-common -y
 sudo apt-add-repository ppa:ansible/ansible -y
 sudo apt-get update
 sudo apt-get install ansible -y
-sudo ansible-galaxy install geerlingguy.pip
-sudo ansible-galaxy install geerlingguy.docker
-sudo ansible-galaxy install geerlingguy.java
-sudo ansible-galaxy install geerlingguy.jenkins
+sudo ansible-galaxy install nickjj.docker --ignore-certs
+sudo ansible-galaxy install geerlingguy.java --ignore-certs
+sudo ansible-galaxy install geerlingguy.jenkins --ignore-certs
