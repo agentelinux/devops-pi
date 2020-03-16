269931472ea95524787d03f59cee00909939deba
agentelinux <krishna@agentelinux.com.br>
2020-03-16 20:11:22 -0300

    #2 update ansible

--- install_ansible.sh
+++ install_ansible.sh
@@ -5,6 +5,6 @@ sudo apt-get install software-properties-common -y
 sudo apt-add-repository ppa:ansible/ansible -y
 sudo apt-get update
 sudo apt-get install ansible -y
-sudo ansible-galaxy install nickjj.docker --ignore-certs
-sudo ansible-galaxy install geerlingguy.java --ignore-certs
-sudo ansible-galaxy install geerlingguy.jenkins --ignore-certs
+sudo ansible-galaxy install nickjj.docker 
+sudo ansible-galaxy install geerlingguy.java 
+sudo ansible-galaxy install geerlingguy.jenkins 
